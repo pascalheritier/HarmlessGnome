3fe2d5fff6f1a2da2203968cfcf037a939796071
pascal.heritier <pascalheritier>
2025-06-15 16:27:03 +0200

    Fix levels zero-ing

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -2,30 +2,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private bool followPlayer;
     [SerializeField] private float speed;
     private Vector2 currentPosition;
     private Vector3 velocity = Vector3.zero;
 
-    [SerializeField] private Transform playerTransform;
-    [SerializeField] private float aheadDistance;
-    [SerializeField] private float speedFollowingPlayer;
-    private float lookAhead;
-
     public void Update()
     {
-        if (!followPlayer)
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosition.x, currentPosition.y, transform.position.z), ref velocity, speed);
-        else
-        {
-            transform.position = new Vector3(playerTransform.position.x + lookAhead, transform.position.y, transform.position.z);
-            lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * playerTransform.localScale.x), speedFollowingPlayer * Time.deltaTime);
-        }
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosition.x, currentPosition.y, transform.position.z), ref velocity, speed);
     }
 
     public void MoveToNewZone(Transform newZone)
     {
-        if(newZone != null)
+        if (newZone != null)
+        {
+            Debug.Log($"Move camera to new zone (X:{newZone.position.x}, Y:{newZone.position.y})");
             currentPosition = newZone.position;
+        }
     }
 }
