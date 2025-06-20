bcf7af0b6987e3c45cc953fe33bee081a665a111
pascal.heritier <pascalheritier>
2025-06-20 09:38:08 +0200

    Add tool location candidates

--- Assets/Scripts/Levels/LevelToolSpawner.cs
+++ Assets/Scripts/Levels/LevelToolSpawner.cs
@@ -1,16 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LevelToolSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private List<ToolCollectible> toolCollectibles;
 
-    // Update is called once per frame
-    void Update()
+
+    [SerializeField]
+    private List<Transform> toolLocationCandidates;
+
+    private void Awake()
     {
-        
+        if (toolLocationCandidates.Count < toolCollectibles.Count)
+            throw new NotSupportedException("Cannot spawn tool collectibles, not enough locations provided!");
+
+        System.Random rng = new();
+        List<Transform> shuffledLocations = toolLocationCandidates.OrderBy(_ => rng.Next()).ToList();
+
+        for (int i = 0; i < toolCollectibles.Count; i++)
+            toolCollectibles[i].transform.position = shuffledLocations[i].transform.position;
     }
 }
